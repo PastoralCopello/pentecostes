b3ac8e85a736f4adb73f7f41bb863e54c1d09136
Nicolás Acosta <nicacosta@frba.utn.edu.ar>
2021-05-12 09:39:42 -0300

    body color for midon

--- mi-don/midon.js
+++ mi-don/midon.js
@@ -2,27 +2,17 @@ const midon = $('.midon')
 const nameLabel = $('.nameLabel')
 const modalTitle = $('.modal-title')
 const modalDescription = $('.modal-description')
-// const modalShareButton = $('.btn-modal-share')
-
-// document.documentElement.style.setProperty('--midon-color', myGift.color)
 loadInfo(myGift)
-// getGift()
-
-function getGift() {
-	changeColor(myGift.color)
-	show(midon)
-	$('.sc__container').style.height = `${
-		scCanvas.getBoundingClientRect().height
-	}px`
-}
 
 function loadInfo(someGift) {
-	// nameLabel.innerText = someGift.name
 	modalDescription.innerText = someGift.description
 	modalTitle.innerText = someGift.name
 }
 
 function scratchCompleted() {
-	document.documentElement.style.setProperty('--midon-full-color', myGift.color)
+	document.documentElement.style.setProperty(
+		'--body-background-color',
+		myGift.color
+	)
 	localStorage.scratchedGift = true
 }
